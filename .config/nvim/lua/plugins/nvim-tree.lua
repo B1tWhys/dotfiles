6c7367f5da701c6a607067937f1382305a6de7a7
B1tWhys <skyarnold1@me.com>
2025-05-09 21:13:18 -0700

    Fixed nvim-tree keybind bug

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -4,24 +4,25 @@ return {
 		config = function()
 			local api = require("nvim-tree.api")
 			local toggleKeybind = "<leader>t"
-			local function on_attach(bufnr)
+			local function my_on_attach(bufnr)
 				api.config.mappings.default_on_attach(bufnr)
 
-				local function opts(desc)
+				local baselineOpts = { buffer = bufnr, silent = true }
+				local function fancyOpts(desc)
 					return { desc = "nvim-tree: " .. desc, buffer = bufnr, silent = true }
 				end
 
-				vim.keymap.set("n", toggleKeybind, api.tree.close, opts("Toggle tree"))
-				vim.keymap.set("n", "t", api.node.open.tab, opts("Open in tab"))
-				vim.keymap.set("n", "h", api.node.open.horizontal, opts("Open in horizontal split"))
-				vim.keymap.set("n", "v", api.node.open.vertical, opts("Open in vertical split"))
-				vim.keymap.del("n", "s", { buffer = bufnr })
-				vim.keymap.set("n", "dd", api.fs.remove)
-				vim.keymap.set("n", "rn", api.fs.rename)
-				vim.keymap.set("n", "<C-n>", api.fs.create)
+				vim.keymap.set("n", toggleKeybind, api.tree.close, fancyOpts("Toggle tree"))
+				vim.keymap.set("n", "t", api.node.open.tab, fancyOpts("Open in tab"))
+				vim.keymap.set("n", "h", api.node.open.horizontal, fancyOpts("Open in horizontal split"))
+				vim.keymap.set("n", "v", api.node.open.vertical, fancyOpts("Open in vertical split"))
+				vim.keymap.del("n", "s", baselineOpts)
+				vim.keymap.set("n", "dd", api.fs.remove, baselineOpts)
+				vim.keymap.set("n", "rn", api.fs.rename, baselineOpts)
+				vim.keymap.set("n", "<C-n>", api.fs.create, baselineOpts)
 			end
 
-			require("nvim-tree").setup({ on_attach = on_attach })
+			require("nvim-tree").setup({ on_attach = my_on_attach })
 
 			vim.keymap.set("n", toggleKeybind, api.tree.focus)
 		end,
