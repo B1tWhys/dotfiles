27671899af297098b12a72fc0a1f8835bd480f4e
B1tWhys <skyarnold1@me.com>
2025-05-17 16:54:56 -0700

    Fix LSP setup

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,36 +1,19 @@
 return {
-	{ "williamboman/mason.nvim", opts = {} },
 	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup()
-			require("mason-lspconfig").setup_handlers({
-				-- Setup automatic configuration of lsp servers
-				-- See h: mason-lspconfig-automatic-server-setup
-				function(server_name)
-					local caps = require("cmp_nvim_lsp").default_capabilities()
-					require("lspconfig")[server_name].setup({
-						capabilities = caps,
-					})
-				end,
-			})
+		"mason-org/mason.nvim",
+		config = function(opts)
+			local mason = require("mason")
+			mason.setup(opts)
+			vim.keymap.set("n", "<leader>M", ":Mason\n")
 		end,
-		dependencies = {
-			"williamboman/mason.nvim",
-			"hrsh7th/cmp-nvim-lsp",
-		},
 	},
-	{ "neovim/nvim-lspconfig", dependencies = { "folke/lazydev.nvim" } },
+	{ "neovim/nvim-lspconfig" },
 	{
-		-- Helps with editing lua neovim files
-		"folke/lazydev.nvim",
-		ft = "lua", -- only load on lua files
-		opts = {
-			library = {
-				-- See the configuration section for more details
-				-- Load luvit types when the `vim.uv` word is found
-				{ path = "luvit-meta/library", words = { "vim%.uv" } },
-			},
+		"mason-org/mason-lspconfig.nvim",
+		config = true,
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"mason-org/mason.nvim",
 		},
 	},
 }
