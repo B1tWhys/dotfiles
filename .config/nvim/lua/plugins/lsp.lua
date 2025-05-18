10280b402478e8fb3e3856fd100cbf0d7894e7f6
B1tWhys <skyarnold1@me.com>
2025-05-17 17:55:53 -0700

    Reenable lazydev to help with editing nvim lua configs

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -16,4 +16,16 @@ return {
 			"mason-org/mason.nvim",
 		},
 	},
+	{
+		-- Helps with editing lua neovim files
+		"folke/lazydev.nvim",
+		ft = "lua", -- only load on lua files
+		opts = {
+			library = {
+				-- See the configuration section for more details
+				-- Load luvit types when the `vim.uv` word is found
+				{ path = "luvit-meta/library", words = { "vim%.uv" } },
+			},
+		},
+	},
 }
