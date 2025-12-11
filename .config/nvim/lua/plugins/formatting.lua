77d1e42e2ba463cd60404a6ddf71ced8668427d8
B1tWhys <skyarnold1@me.com>
2025-12-10 18:18:44 -0800

    Fix annoying python popup when saving

--- .config/nvim/lua/plugins/formatting.lua
+++ .config/nvim/lua/plugins/formatting.lua
@@ -2,10 +2,20 @@ return {
 	{
 		"mhartington/formatter.nvim",
 		config = function()
+			local function quiet(formatter_fn)
+				return function(...)
+					local conf = formatter_fn(...)
+					if type(conf) == "table" then
+						conf.ignore_exitcode = true
+					end
+					return conf
+				end
+			end
+
 			require("formatter").setup({
 				filetype = {
 					lua = require("formatter.filetypes.lua").stylua,
-					python = require("formatter.filetypes.python").black,
+					python = quiet(require("formatter.filetypes.python").black),
 					go = require("formatter.filetypes.go").goimports,
 					javascript = require("formatter.filetypes.javascript").prettier,
 					html = require("formatter.filetypes.html").prettier,
